71df1190d3e664d60011960c15514fbf08dc71a5
Ariton84 <vanver@gmail.com>
2016-12-17 19:47:07 -0800

    Add files via upload

---
+++ ITC110Assignment8/ITC110Assignment8/Bill.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITC110Assignment8
+{
+    public class Bill
+    {
+        double tax = .09;
+        public Bill()
+        {
+            Amount = 0;
+            TipPercent = 0;
+        }
+        public Bill(double amount, double tipPercent)
+        {
+            Amount = amount;
+            TipPercent = tipPercent;
+        }
+        public double Amount { get; set; }
+        public double TipPercent { get; set; }
+
+        public double Tax()
+        {
+            double Tax = tax * Amount;
+            return Tax; 
+        }
+        public double CalcTip()
+        {
+            double calctip = Amount * TipPercent;
+            return calctip;
+        }
+        public double CalcTotal()
+        {
+            double calcTotal = (Amount * TipPercent) + (Amount * .09) + Amount;
+            return calcTotal;
+        }
+        
+       
+    }
+}
---
+++ ITC110Assignment8/ITC110Assignment8/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITC110Assignment8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    
+
+        private void buttonCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            Bill b = new ITC110Assignment8.Bill();
+            b.Amount = double.Parse(amount.Text);
+
+            double tip = 0;
+
+            if (tip_10.IsChecked == true)
+            {
+                b.TipPercent = .1;
+            }
+
+            else if (tip_15.IsChecked == true)
+            {
+                b.TipPercent = .15;
+            }
+
+            else if (tip_20.IsChecked == true)
+            {
+                b.TipPercent = .2;
+            }
+            else
+            {
+                b.TipPercent = double.Parse(textOther.Text)/100;               
+            }
+
+
+            labelResults.Content = "Your tax is " + b.Tax().ToString() + ", your tip is " + b.CalcTip().ToString() + ", and your total is " + b.CalcTotal().ToString();
+
+
+
+        }
+
+    }
+}
